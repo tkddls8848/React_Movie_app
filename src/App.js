f5288be1af271e738feba36128cbeb9b9b6ab603
tkddls8848 <tkddls8848@gmail.com>
2019-03-27 13:36:40 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie'
+
+const movies = [
+  {
+    title : "Matrix",
+    image : "https://images-na.ssl-images-amazon.com/images/I/51vpnbwFHrL.jpg"
+  },
+  {
+    title : "Full Metal Jacket",
+    image : "https://images-na.ssl-images-amazon.com/images/I/91sTE00%2Bm%2BL._SY606_.jpg"
+  },
+  {
+    title : "Old boy",
+    image : "http://www.cinecafe.kr/files/attach/images/12486/035/056/5480dc8794a244781703283a4c66800d.jpg"
+  },
+  {
+    title : "Star wars",
+    image : "https://cdn-images-1.medium.com/max/1920/1*S7LfTcdi4uLWmbuueEIycQ.jpeg"
+  }
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {movies.map((movie, index) => {
+            return <Movie title={movie.title} poster={movie.image} key={index}/>
+        })}
       </div>
     );
   }
