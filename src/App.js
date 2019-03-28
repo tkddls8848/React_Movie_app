bfff7eeae1d8d840724699288c2e065db8900069
tkddls8848 <tkddls8848@gmail.com>
2019-03-28 15:30:03 +0900

    json 통신, async await, yarn build

--- src/App.js
+++ src/App.js
@@ -5,63 +5,67 @@ import Movie from './Movie'
 class App extends Component {
 
   state = {
-    greeting : ""
+    greeting : 'Loading',
+    time : 500
   }
 
-
-
   componentDidMount(){
+
+    // setTimeout(() => {
+    //   this._setGreeting()
+    //   setTimeout(() => {
+    //     this._setGreeting()
+    //     setTimeout(() => {
+    //       this._setGreeting()
+    //       setTimeout(() => {
+    //         this._setGreeting()
+    //       }, 1000)
+    //     }, 1000)
+    //   }, 1000)
+    // }, 1000)
+
+    this._setTimeout()
+
     setTimeout(() => {
-      this.setState({
-        greeting : "Loading."
-      })
-      setTimeout(() => {
-        this.setState({
-          greeting : "Loading.."
-        })
-        setTimeout(() => {
-          this.setState({
-            greeting : "Loading..."
-          })
-          setTimeout(() => {
-            this.setState({
-              greeting : "Loading...."
-            })
-          }, 1000)
-        }, 1000)
-      }, 1000)
-    }, 1000)
-    setTimeout(() => {
-      this.setState({
-        movies : [
-          {
-            title : "Matrix",
-            image : "https://images-na.ssl-images-amazon.com/images/I/51vpnbwFHrL.jpg"
-          },
-          {
-            title : "Full Metal Jacket",
-            image : "https://images-na.ssl-images-amazon.com/images/I/91sTE00%2Bm%2BL._SY606_.jpg"
-          },
-          {
-            title : "Old boy",
-            image : "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/6rIc/image/gCQXUtxPRSKlcl8QbUzOIUtLpxI"
-          },
-          {
-            title : "Star wars",
-            image : "http://moonhak.co.kr/home/wp-content/uploads/bookcover/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88-%EC%94%A8%EB%84%A4%EC%95%84%ED%8A%B84_%ED%91%9C1_web.jpg"
-          },
-          {
-            title : "new movie",
-            image: "https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg"
-          }
-        ]
-      })
+      this.setState(
+        this._getMovies()
+      )
     }, 5000)
   }
 
+  _setTimeout = (time) => {
+    let interval = time + 500
+    setTimeout(() => {
+      this._setGreeting()
+    }, interval)
+  }
+
+  _setGreeting = () => {
+    this._setTimeout(this.state.time)
+    let str = this.state.greeting
+    let greet = str + "."
+    this.setState({
+      greeting : greet
+    })
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies 
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then((response) => response.json())
+    .then((response) => response.data.movies)
+    .catch((err) => console.log(err))
+  }
+
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.image} key={index}/>
+    const movies = this.state.movies.map((movie) => {
+      return <Movie title={movie.title} poster={movie.medium_cover_image} key={movie.id} genres={movie.genres} synopsis={movie.synopsis}/>
     })
     return movies
   }
@@ -72,8 +76,9 @@ class App extends Component {
   }
 
   render() {
+    const {movies} = this.state
     return (
-      <div className="App">
+      <div className={movies ? "App" : "App--loading"}>
         {this.state.movies ? this._renderMovies() : this._renderGreeting()}
       </div>
     );
--- src/Movie.js
+++ src/Movie.js
@@ -1,35 +1,82 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import React from 'react';
 import './Movie.css';
+import LinesEllipsis from 'react-lines-ellipsis'
 
-class Movie extends Component {
+// class Movie extends Component {
 
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
+//     static propTypes = {
+//         title: PropTypes.string.isRequired,
+//         poster: PropTypes.string.isRequired
+//     }
 
-    render(){
-        return(
-            <div>
-                <h1>{this.props.title}</h1>
-                <MoviePoster poster={this.props.poster}/>
+//     render(){
+//         return(
+//             <div>
+//                 <h1>{this.props.title}</h1>
+//                 <MoviePoster poster={this.props.poster}/>
                 
+//             </div>
+//         ) 
+//     }
+// }
+
+// class MoviePoster extends Component {
+
+//     static propTypes = {
+//         poster: PropTypes.string.isRequired
+//     }
+
+//     render(){
+//         return(
+//             <img src={this.props.poster} alt="movie poster"/>
+//         )
+//     }
+// }
+
+function Movie({title, poster, genres, synopsis}){
+    return(
+        <div className="Movie">
+            <div className="Movie__Column">
+                <MoviePoster poster={poster} alt={title}/>
             </div>
-        ) 
-    }
+            <div className="Movie__Column">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genres, index) => {
+                        return <MovieGenres genres={genres} key={index}/>
+                        }
+                    )}
+                </div>
+                <div className="Movie__Synopsis">
+                    <MovieSynopsis synopsis={synopsis}/>
+                </div>
+            </div>
+        </div>
+    ) 
 }
 
-class MoviePoster extends Component {
-
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    }
-
-    render(){
-        return(
-            <img src={this.props.poster} alt="movie poster"/>
-        )
-    }
+function MoviePoster({poster}){
+    return(
+        <img src={poster} alt="movie poster"/>
+    )
 }
+
+function MovieGenres({genres}){
+    return(
+        <span className="Movie_Genre">{genres}</span>
+    )
+}
+
+function MovieSynopsis({synopsis}){
+    return(
+        <LinesEllipsis
+        text={synopsis}
+        maxLine='3'
+        ellipsis='......'
+        trimRight
+        basedOn='letters'
+        /> 
+    )
+}
+
 export default Movie;
